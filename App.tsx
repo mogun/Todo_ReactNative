f533e21db8f60ce4fc8cb466517eb3d61d5011dc
moguo <mogu.mogu.mogun@gmail.com>
2025-04-27 17:57:26 +0900

    レビュー修正: データ永続化対応

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { TodoList } from './app/components/TodoList';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Todo } from './app/types/todo';
 import { v4 as uuidv4 } from 'uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const TODOS_STORAGE_KEY = '@todos';
 
 export default function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      const storedTodos = await AsyncStorage.getItem(TODOS_STORAGE_KEY);
+      if (storedTodos) {
+        setTodos(JSON.parse(storedTodos));
+      }
+    }
+    loadTodos();
+  }, []);
+
+  useEffect(() => {
+    const saveTodos = async () => {
+      await AsyncStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
+    }
+    saveTodos();
+  }, [todos]);
 
   const handleToggle = (id: string) => {
     setTodos(todos.map((todo) => todo.id === id ? {...todo, completed: !todo.completed} : todo));
